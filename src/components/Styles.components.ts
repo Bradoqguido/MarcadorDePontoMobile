b410726177e001abcbb4e988eefdaa2d6caa1513
Jeferson Eduardo Guido <jefersoneduardoguido@gmail.com>
2022-05-17 13:37:23 -0300

    Avoid compilation errors

--- src/components/Styles.components.ts
+++ src/components/Styles.components.ts
@@ -1,19 +1,19 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-const ButtonContainer = styled.TouchableOpacity`
-    margin-vertical: 40px;
-    width: 120px;
-    height: 40px;
-    padding: 12px;
-    border-radius: 10px;
-    background-color: ${props => props.bgColor}
-`;
+// const ButtonContainer = styled.TouchableOpacity`
+//     margin-vertical: 40px;
+//     width: 120px;
+//     height: 40px;
+//     padding: 12px;
+//     border-radius: 10px;
+//     background-color: ${props => props.bgColor}
+// `;
 
-const ButtonText = styled.Text`
-    font-size: 16px;
-    text-align: center;
-`;
+// const ButtonText = styled.Text`
+//     font-size: 16px;
+//     text-align: center;
+// `;
 
 // export const CButton = ({ onPress, bgColor, title}) => {
 //     return (
