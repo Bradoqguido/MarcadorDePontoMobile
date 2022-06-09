376830b61fb52412fec4f609c48b1ee96af8eae4
Jeferson Eduardo Guido <jefersoneduardoguido@gmail.com>
2022-06-09 15:10:30 -0300

    Add: Sample data to test

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
 import * as React from 'react';
+import { setConfig, addTimeRegister } from './src/Helpers/Utils';
 import Routes from './src/pages/Routes';
 
+import dateTimeSamples from './src/Helpers/dateTimeSamples.json';
+
 export default function App() {
+
+  setConfig({
+    dtaHorarioInicioExpedinte: new Date(dateTimeSamples.config.dtaHorarioInicioExpedinte),
+    dtaHorarioFimExpediente: new Date(dateTimeSamples.config.dtaHorarioFimExpediente),
+    dtaHorarioInicioAlmoco: new Date(dateTimeSamples.config.dtaHorarioInicioAlmoco),
+    dtaHorarioFimAlmoco: new Date(dateTimeSamples.config.dtaHorarioFimAlmoco)
+  });
+
+  dateTimeSamples.lstPontos.forEach(e => {
+    addTimeRegister({
+      id: e.id,
+      tipo: e.tipo,
+      dataHora: new Date(e.dataHora),
+    });
+  });
+
   return (
     <>
       <Routes />
